bf3cf0251b7fb305a1d5d7c6d8095361cd7b55bd
Abhishek Santra <abhisheksantra28@gmail.com>
2023-12-07 12:20:55 +0530

    Files added

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="w-60 h-72 rounded-[18px] bg-zinc-900/40">
+        icon 
+        text
+        Footer 
+            data
+            tag
+    </div>
+  )
+}
+
+export default Card
