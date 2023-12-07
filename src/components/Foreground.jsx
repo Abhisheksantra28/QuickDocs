76c948ea3475707bb7d094a0c4d1fa06c90108f7
Abhishek Santra <abhisheksantra28@gmail.com>
2023-12-07 14:16:37 +0530

    Added more cards

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
+
 const Foreground = () => {
+    const ref = useRef(null);
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: "0.9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
+
   return (
-    <div className="fixed z-3 w-full h-full">
-      <Card />
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+      {data.map((item,index)=>(
+        <Card data={item} reference={ref}/>
+      ))}
     </div>
   );
 };
